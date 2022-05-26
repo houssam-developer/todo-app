a05c77efcb187345874ae3904a55a8cf48acd719
houssam-dev <developpeur.pro.ma@gmail.com>
2022-05-26 14:11:04 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useEffect, useRef, useState } from 'react';
+import Footer from './components/Footer'
+
+
+function App() {
+
+	return (
+		<div className="">
+			<h1>Hello world!</h1>
+			<Footer></Footer>
+		</div>
+	)
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+	return (
+		<p className="created-by">created by <span>houssam-developer</span> - devChallenges.io</p>
+	)
+}
+
+export default Footer
